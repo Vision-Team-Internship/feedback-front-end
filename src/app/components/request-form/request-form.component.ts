a7841451903b0a1f8d3aa0c4f864a26db1520676
Sivcheu <choeng.sivcheu02@gmail.com>
2022-02-04 22:05:08 +0700

    request form

--- src/app/components/request-form/request-form.component.ts
+++ src/app/components/request-form/request-form.component.ts
@@ -1,17 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { AdminService } from 'src/app/services/admin/admin.service';
+import { Service } from 'src/app/services/service.service';
 
 @Component({
   selector: 'app-request-form',
   templateUrl: './request-form.component.html',
-  styleUrls: ['./request-form.component.css']
+  styleUrls: ['./request-form.component.css'],
 })
 export class RequestFormComponent implements OnInit {
+  constructor(
+    private adminService: AdminService,
+    private fb: FormBuilder,
+    private service: Service
+  ) {}
 
-  constructor(private adminService: AdminService) { }
-  data: any = this.adminService.user
+  requestForm: FormGroup = new FormGroup({});
+  data: any = this.adminService.user;
   ngOnInit(): void {
-    console.log(this.data)
+    console.log(this.data);
+    this.requestForm = this.fb.group({
+      title: new FormControl(''),
+      message: new FormControl(''),
+    });
   }
+  get f() {
+    return this.requestForm.controls;
+  }
+  sendRequest() {
+    const data: any = {
+      title: this.f.title.value,
+      message: this.f.message.value,
+      type: 'request',
+    };
 
+    this.service.sendMessage(data).subscribe((res) => {
+      console.log(res);
+      this.requestForm.reset;
+    });
+  }
 }
